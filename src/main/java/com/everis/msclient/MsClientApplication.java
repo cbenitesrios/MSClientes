ae605849e731599ac210d151b7c43329e2452488
cbenites <cbenites@LIM-65QG5Y2.usersad.everis.int>
2020-07-16 15:28:57 -0500

    gateway config

--- src/main/java/com/everis/msclient/MsClientApplication.java
+++ src/main/java/com/everis/msclient/MsClientApplication.java
@@ -2,10 +2,13 @@ package com.everis.msclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.EnableWebFlux; 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableWebFlux
 public class MsClientApplication {
 
   public static void main(final String[] args) {
